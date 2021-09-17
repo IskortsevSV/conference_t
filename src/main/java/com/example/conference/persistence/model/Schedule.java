4fcfb3851292c4dd3a3640ea3a517a1bb0fe4e51
Stanislav Iskortsev <iskortsevsv@gmail.com>
2021-09-17 22:22:11 +0200

    create entity Schedule

---
+++ src/main/java/com/example/conference/persistence/model/Schedule.java
@@ -0,0 +1,51 @@
+package com.example.conference.persistence.model;
+
+import lombok.Data;
+
+import javax.persistence.*;
+import java.util.Date;
+
+/**
+ * by Iskortsev S.V.
+ */
+
+@Data
+@Entity
+@Table(name = "schedules")
+public class Schedule {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private int id;
+    @Column(name = "audience")
+    private int audience;
+    @Column(name = "date_time_start")
+    private Date dateTimeStart;
+    @Column(name = "report")
+    private int report;
+
+    @Transient
+    private String audienceName;
+    @Transient
+    private String reportName;
+    @Transient
+    private String userName;
+
+    public Schedule() {
+    }
+
+    public Schedule(int audience, Date dateTimeStart, int report) {
+        this.audience = audience;
+        this.dateTimeStart = dateTimeStart;
+        this.report = report;
+    }
+
+    public Schedule(int id,Date dateTimeStart, String audienceName, String reportName, String userName) {
+        this.id = id;
+        this.dateTimeStart = dateTimeStart;
+        this.audienceName = audienceName;
+        this.reportName = reportName;
+        this.userName = userName;
+    }
+}
