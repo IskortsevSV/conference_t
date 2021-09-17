09f6c8fa289b549874135b2599af07685538803c
Stanislav Iskortsev <iskortsevsv@gmail.com>
2021-09-18 00:37:39 +0200

    Create security config MySecurityInitializer

---
+++ src/main/java/com/example/conference/configuration/MySecurityInitializer.java
@@ -0,0 +1,11 @@
+package com.example.conference.configuration;
+
+import org.springframework.security.web.context.AbstractSecurityWebApplicationInitializer;
+
+/**
+ * by Iskortsev S.V.
+ */
+
+public class MySecurityInitializer extends AbstractSecurityWebApplicationInitializer {
+
+}
