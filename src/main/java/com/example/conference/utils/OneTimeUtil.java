76798be3cac016b006ae8a5465729d3be42b5b71
Stanislav Iskortsev <iskortsevsv@gmail.com>
2021-09-19 00:34:17 +0200

    Create util class OneTimeUtil

---
+++ src/main/java/com/example/conference/utils/OneTimeUtil.java
@@ -0,0 +1,30 @@
+package com.example.conference.utils;
+
+import com.example.conference.persistence.dao.service.intefaces.SchedulesService;
+import com.example.conference.persistence.model.Schedule;
+
+import java.util.List;
+
+/**
+ * by Iskortsev S.V.
+ */
+
+public class OneTimeUtil {
+
+    public static boolean doNotEqualsOneTime(Schedule schedule, SchedulesService schedulesService) {
+        List<Schedule> allSchedules = schedulesService.getAll();
+        String checkDateTime = schedule.getDateTimeStart();
+        int checkAudience = schedule.getAudience();
+        boolean result = false;
+
+        for (Schedule allSchedule : allSchedules) {
+            String dateTimeStart = allSchedule.getDateTimeStart();
+            int audience = allSchedule.getAudience();
+            if (dateTimeStart.equals(checkDateTime) && audience == checkAudience) {
+                result = true;
+            }
+        }
+        return result;
+    }
+
+}
