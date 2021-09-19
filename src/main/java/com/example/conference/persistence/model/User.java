ca270b0d4746cd0f291ad09b2ad608f3b13886f4
Stanislav Iskortsev <iskortsevsv@gmail.com>
2021-09-19 17:06:14 +0200

    Change User class add field in future make OneToOne

--- src/main/java/com/example/conference/persistence/model/User.java
+++ src/main/java/com/example/conference/persistence/model/User.java
@@ -16,13 +16,19 @@ public class User {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "id")
-    private int id;
+    private long id;
     @Column(name = "username")
     private String userName;
     @Column(name = "password")
     private String userPassword;
     @Column(name = "enabled")
     private String enabled;
+
+    // @OneToOne(mappedBy = "user", cascade = {CascadeType.ALL})
+    @Transient
+    private Authority authority;
+
+
 
     public User() {
     }
