68111bd2a07c4ebfa45011f0840183e5a748edc4
Stanislav Iskortsev <iskortsevsv@gmail.com>
2021-09-20 22:38:18 +0200

    Add JDBC Authentication in MySecurityConfig

--- src/main/java/com/example/conference/configuration/MySecurityConfig.java
+++ src/main/java/com/example/conference/configuration/MySecurityConfig.java
@@ -2,50 +2,46 @@ package com.example.conference.configuration;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+
+import javax.annotation.Resource;
 import javax.sql.DataSource;
 
 /**
  * by Iskortsev S.V.
  */
-
+@Configuration
 @EnableWebSecurity
 public class MySecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Autowired
     DataSource dataSource;
 
-   /* @Override
-    protected void configure(HttpSecurity security) throws Exception
-    {
-        security.httpBasic().disable();
-    }*/
-
+    @Resource(name = "myUserDetailService")
+    UserDetailsService userDetailService;
 
     @Bean
     public PasswordEncoder encoder() {
         return new BCryptPasswordEncoder();
     }
-
-
-
-   /* @Bean
+    @Bean
     public DaoAuthenticationProvider authProvider() {
         DaoAuthenticationProvider authProvider = new DaoAuthenticationProvider();
         authProvider.setUserDetailsService(userDetailService);
         authProvider.setPasswordEncoder(encoder());
         return authProvider;
-    }*/
-
+    }
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
         auth.jdbcAuthentication().dataSource(dataSource);
-
     }
     @Override
     protected void configure(HttpSecurity http) throws Exception {
@@ -53,9 +49,15 @@ public class MySecurityConfig extends WebSecurityConfigurerAdapter {
                 .antMatchers("/speaker/**").hasRole("SPEAKER")
                 .antMatchers("/admin/**").hasRole("ADMIN")
                 .and().formLogin().permitAll(); // форма логина и пароля будет запрашиватся у всех
-
        // .antMatchers("/manager_info/**").hasRole("MANAGER");
     }
 
+    // на время тестов
+     /*  @Override
+    protected void configure(HttpSecurity security) throws Exception
+    {
+        security.httpBasic().disable();
+    }
+*/
 
 }
