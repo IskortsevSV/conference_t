f79ba0306adc78b4ab12a5d9643f9f180e354544
Stanislav Iskortsev <iskortsevsv@gmail.com>
2021-09-20 22:39:34 +0200

    Create MyUserDetailService to Authentication

---
+++ src/main/java/com/example/conference/configuration/MyUserDetailService.java
@@ -0,0 +1,30 @@
+package com.example.conference.configuration;
+
+import com.example.conference.persistence.dao.service.intefaces.UserService;
+import com.example.conference.persistence.model.User;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+import static org.springframework.security.core.userdetails.User.withUsername;
+
+/**
+ * by Iskortsev S.V.
+ */
+@Service("myUserDetailService")
+public class MyUserDetailService implements UserDetailsService {
+
+    @Autowired
+    private UserService userService;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        final User user = userService.getUserByName(username);
+        if (user == null) {
+            throw new UsernameNotFoundException(username);
+        }
+        UserDetails userDetails = withUsername(user.getUserName()).password(user.getUserPassword()).build();
+        return userDetails;
+    }
+}
