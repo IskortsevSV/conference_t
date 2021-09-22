5727327f40a74f5b412ae5bacaf1aedfdb38c03b
Stanislav Iskortsev <iskortsevsv@gmail.com>
2021-09-22 23:30:07 +0200

    Add room OneToMany with schedules

--- src/main/java/com/example/conference/persistence/model/Room.java
+++ src/main/java/com/example/conference/persistence/model/Room.java
@@ -3,6 +3,7 @@ package com.example.conference.persistence.model;
 import lombok.Data;
 
 import javax.persistence.*;
+import java.util.List;
 
 /**
  * by Iskortsev S.V.
@@ -19,6 +20,10 @@ public class Room {
 
     @Column(name = "audience")
     private String audience;
+
+    @OneToMany(cascade = {CascadeType.REFRESH,CascadeType.PERSIST,CascadeType.DETACH,CascadeType.MERGE}
+            ,mappedBy = "rooms") // это связь ищи в классе department
+    private List<Schedule> scheduleList;
 
     public Room() {
     }
