1b5e608fd1362b4bb2ddac1e57cfdd6ee4af2f95
Stanislav Iskortsev <iskortsevsv@gmail.com>
2021-09-22 23:29:17 +0200

    Refactoring ListenerController

--- src/main/java/com/example/conference/controllers/ListenerController.java
+++ src/main/java/com/example/conference/controllers/ListenerController.java
@@ -14,9 +14,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
 
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpSession;
-import java.util.Comparator;
 import java.util.List;
 
 /**
