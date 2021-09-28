9d786a91f26fbb012f97ba20aff0afa69622d41d
Stanislav Iskortsev <iskortsevsv@gmail.com>
2021-09-28 12:28:50 +0200

    Change method in OneTimeUtil

--- src/main/java/com/example/conference/utils/OneTimeUtil.java
+++ src/main/java/com/example/conference/utils/OneTimeUtil.java
@@ -3,6 +3,12 @@ package com.example.conference.utils;
 import com.example.conference.persistence.dao.service.intefaces.SchedulesService;
 import com.example.conference.persistence.model.Schedule;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.time.LocalTime;
+import java.time.format.DateTimeFormatter;
+import java.time.temporal.ChronoField;
+import java.util.Date;
 import java.util.List;
 
 /**
@@ -11,20 +17,35 @@ import java.util.List;
 
 public class OneTimeUtil {
 
-    public static boolean doNotEqualsOneTime(Schedule schedule, SchedulesService schedulesService) {
+    private static final SimpleDateFormat SDF = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+    private static final long HOUR = 3600000L;
+
+    public static boolean checkBothEqualsTimeAndAudience(Schedule schedule, SchedulesService schedulesService) {
         List<Schedule> allSchedules = schedulesService.getAll();
         String checkDateTime = schedule.getDateTimeStart();
         long checkAudience = schedule.getAudience();
-        boolean result = false;
 
-        for (Schedule allSchedule : allSchedules) {
-            String dateTimeStart = allSchedule.getDateTimeStart();
-            long audience = allSchedule.getAudience();
-            if (dateTimeStart.equals(checkDateTime) && audience == checkAudience) {
-                result = true;
+
+        try {
+            Date checkDate = SDF.parse(checkDateTime);
+            long a = checkDate.getTime();
+
+            for (Schedule allSchedule : allSchedules) {
+                String dateTime = allSchedule.getDateTimeStart();
+                long b = SDF.parse(dateTime).getTime();
+                long audience = allSchedule.getAudience();
+                long range = Math.abs(a - b);
+                if (range <= HOUR && audience == checkAudience) {
+                    return true;
+                }
             }
+        } catch (ParseException e) {
+            e.printStackTrace();
         }
-        return result;
+
+
+        return false;
     }
+
 
 }
